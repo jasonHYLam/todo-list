aadfd77648ac6221caf27daf28c79cf890a66aed
jaleel <jasonlamdarksouls@gmail.com>
2022-08-15 13:51:50 +0100

    Get Response From Submit Todo Button
    Using event delegation.

--- src/displayForm.js
+++ src/displayForm.js
@@ -4,8 +4,29 @@ const displayTodoForm = () => {
     const todoForm = document.createElement('form');
 
     const titleInput = document.createElement('input');
+    titleInput.setAttribute('placeholder', 'task: ')
+
+
+    const descInput = document.createElement('input');
+    descInput.setAttribute('placeholder', 'description: ')
+
+    const dueDate = document.createElement('input');
+    dueDate.setAttribute('placeholder', 'due date: ')
+
+    const submitTodoButton = document.createElement('button');
+    submitTodoButton.setAttribute('type', 'button');
+    submitTodoButton.id = 'submit-todo-button';
+    submitTodoButton.textContent = 'add';
+
+    const cancelButton = document.createElement('button');
+    cancelButton.textContent = 'x';
+
 
     todoForm.appendChild(titleInput);
+    todoForm.appendChild(descInput);
+    todoForm.appendChild(dueDate);
+    todoForm.appendChild(submitTodoButton);
+    todoForm.appendChild(cancelButton);
 
     wrapper.appendChild(todoForm);
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ createToDo();
 const addTodoButton = document.getElementById('add-todo-button');
 console.log(addTodoButton);
 
+const submitTodoButton = document.getElementById('submit-todo-button');
 addTodoButton.addEventListener('click', (e) => {
-
     displayTodoForm();
+})
+
+//click on submit todo button
+document.addEventListener('click', (e)=> {
+    console.log(e.target);
+    if (e.target.id != 'submit-todo-button') {
+        return;}
+
+    console.log('hehe clicj');
 })
